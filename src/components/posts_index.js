dd90f4299c5c8369c2befd84e4724e541198ff41
John Chang <johnchang9094@gmail.com>
2017-04-02 21:00:28 +0100

    Rendering Posts to the screen, along with content

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,6 +8,17 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map( (post) => {
+      return (
+          <li className="list-group-item" key={post.id}>
+          <span className="pull-xs-right">{post.categories}</span>
+          <strong>{post.title}</strong>
+          </li>
+        );
+    });
+  }
+
   render() {
     return (
         <div>
@@ -16,12 +27,19 @@ class PostsIndex extends Component {
           Add Post
           </Link>
         </div>
-        List of Blog Posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
         </div>
       );
   }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+
 // This givues as access to this.props.fetchPosts and now we can 
 // call it inside componentWillMount()
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 
 class PostsNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
+  onSubmit(props) {
+    this.props.createPost(props)
+    .then( () => { 
+    // blog post has been created, navigate the user back to the index
+    // We navigate by calling this.contenxt.router.push with the
+    // nwe path to navigate to.
+    this.context.router.push('/');
+    });
+  }
 
   render() {
     const { fields: { title, categories, content }, handleSubmit } = this.props;
     // const title = this.props.fields.title;
     // same as const handleSubmit = this.props.handleSubmit
     return (
-        <form onSubmit={handleSubmit(this.props.createPost)}>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
           <h3>Create a new post</h3>
-          <div className="form-group">
+
+          <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : '' }`}>
             <label>Title</label>
               <input type="text" className="form-control" {...title} />
               <div className="text-help">
@@ -20,23 +35,24 @@ class PostsNew extends Component {
               </div>
           </div>
 
-           <div className="form-group">
+          <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : '' }`}>
             <label>Categories</label>
               <input type="text" className="form-control" {...categories} />
-              <div className="categories-help">
+              <div className="text-help">
               {categories.touched ? categories.error : ''}
               </div>
           </div>
           
-          <div className="form-group">
+          <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : '' }`}>
             <label>Content</label>
               <textarea className="form-control" {...content} />
-              <div className="content-help">
+              <div className="text-help">
               {content.touched ? content.error : ''}
               </div>
           </div>
           
           <button type="submit" className="btn btn-primary">Submit</button>
+          <Link to="/" className="btn btn-danger">Cancel</Link>
         </form>
       );
   }
@@ -46,7 +62,7 @@ function validate(values) {
   const errors = {};
 
   if (!values.title || values.title.trim() === '') {
-    errors.title = 'Enter a username please';
+    errors.title = 'Please enter a username';
   }
   if (!values.categories || values.categories.trim() === '') {
     errors.categories = "Please enter a category";
